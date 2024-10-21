03ce8bc190398116b46a4c86ad58e4055379e863
Guilherme <guisouzaskill@gmail.com>
2024-10-20 22:14:19 -0300

    escope the project

---
+++ backend/src/main/java/com/CEFET/atvRodrigo/AtvRodrigoApplication.java
@@ -0,0 +1,13 @@
+package com.CEFET.atvRodrigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvRodrigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvRodrigoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/CEFET/atvRodrigo/AtvRodrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CEFET.atvRodrigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvRodrigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
