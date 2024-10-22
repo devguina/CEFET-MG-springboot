6205f86665a1529ca7bdca4dac7d04f3debd215a
Guilherme <guisouzaskill@gmail.com>
2024-10-22 03:39:41 -0300

    remove dependencies flyway | CRUD for model User

--- backend/src/main/java/com/CEFET/atvRodrigo/controllers/UserController.java
+++ backend/src/main/java/com/CEFET/atvRodrigo/controllers/UserController.java
@@ -3,26 +3,69 @@ package com.CEFET.atvRodrigo.controllers;
 import com.CEFET.atvRodrigo.RecordsDTO.UserRecordDTO;
 import com.CEFET.atvRodrigo.models.User;
 import com.CEFET.atvRodrigo.services.UserService;
-import jakarta.persistence.Id;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/user")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @PostMapping(value = "/post")
-    public ResponseEntity<User> postUser(@RequestBody @Valid UserRecordDTO dto){
+    //POST
+    @PostMapping(value = "/")
+    public ResponseEntity<User> postUser(@RequestBody @Valid UserRecordDTO dto) {
         User user = userService.addUser(dto);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
+
+    //GET ALL
+    @GetMapping(value = "/")
+    public ResponseEntity<List<User>> getAllUser() {
+        List<User> allUsers = userService.allUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    //GET BY ID
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> getUser(@PathVariable UUID id) {
+        Optional<User> userOptional = userService.getUser(id);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.ok(userOptional.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found by id: " + id);
+        }
+    }
+
+    //PUT BY ID
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<?> putUser(@PathVariable UUID id,
+                                     @RequestBody @Valid UserRecordDTO dto) {
+        User user = userService.updateUser(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+
+    //DELET BY ID
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable UUID id) {
+        Optional<User> userOptional = userService.getUser(id);
+        if (userOptional.isPresent()) {
+            userService.deleteUser(id);
+            return ResponseEntity.status(HttpStatus.OK).body("User exclued successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not Found by id: " + id);
+        }
+
+    }
 }
+
+
--- backend/src/main/java/com/CEFET/atvRodrigo/services/UserService.java
+++ backend/src/main/java/com/CEFET/atvRodrigo/services/UserService.java
@@ -5,7 +5,9 @@ import com.CEFET.atvRodrigo.models.User;
 import com.CEFET.atvRodrigo.repositories.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,36 +20,38 @@ public class UserService {
     private UserRepository repository;
 
     //POST
-    public User addUser(UserRecordDTO dto){
+    public User addUser(UserRecordDTO dto) {
         User user = new User();
         BeanUtils.copyProperties(dto, user);
         user.setActive(true);
         return repository.save(user);
     }
 
-    public List<User> allUsers(){
+    public List<User> allUsers() {
         return repository.findAll();
     }
 
-    public Optional<User> getUser(UUID id){
+    public Optional<User> getUser(UUID id) {
+        Optional<User> userOptional = repository.findById(id);
         return repository.findById(id);
     }
 
-    public void deleteUser(UUID id){
+    public void deleteUser(UUID id) {
         repository.deleteById(id);
     }
 
-    public User updateUser(UUID id, UserRecordDTO dto){
+    public User updateUser(UUID id, UserRecordDTO dto) {
         Optional<User> userOptional = repository.findById(id);
         if (userOptional.isPresent()){
-            User user = new User();
-            user.setName(user.getName());
-            user.setLogin(user.getLogin());
-            user.setPassword(user.getPassword());
-            return repository.save(user);
+            User user1 = new User();
+            BeanUtils.copyProperties(dto, user1);
+            user1.setName(user1.getName());
+            user1.setLogin(user1.getLogin());
+            user1.setPassword(user1.getPassword());
+            return repository.save(user1);
         }
-        else {
-            throw new RuntimeException("Client not found id: "+id);
+        else  {
+             throw new RuntimeException("User not found by id: "+id);
         }
     }
 }
